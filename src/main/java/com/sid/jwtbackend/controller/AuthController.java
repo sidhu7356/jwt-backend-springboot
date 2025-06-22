589e94adb2a7456283692d09af9e67e82893dc3f
sidhu7356 <sidharthc7356@gmail.com>
2025-06-22 13:09:43 +0530

    added AuthController and yaml configs

---
+++ src/main/java/com/sid/jwtbackend/controller/AuthController.java
@@ -0,0 +1,29 @@
+package com.sid.jwtbackend.controller;
+
+import com.sid.jwtbackend.dto.AuthRequest;
+import com.sid.jwtbackend.dto.AuthResponse;
+import com.sid.jwtbackend.service.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/register")
+    public ResponseEntity<String> register(@RequestBody AuthRequest authRequest) {
+        return ResponseEntity.ok(authService.register(authRequest));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponse> login(@RequestBody AuthRequest authRequest) {
+        return ResponseEntity.ok(authService.login(authRequest));
+    }
+}
