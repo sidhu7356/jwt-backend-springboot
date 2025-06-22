4cd6f0e63ba281d03a10d6624ca99c963351ff05
sidhu7356 <sidharthc7356@gmail.com>
2025-06-22 09:48:44 +0530

    Initial Spring Boot Project Setup with Gradle

---
+++ src/test/java/com/sid/jwtbackend/JwtbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sid.jwtbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
